a65fc2572467f79539dc6d2b7f336b614d4c81cc
GTL1970 <92163683+GTL1970@users.noreply.github.com>
2021-11-23 20:39:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Defining the function
+const pizzaMaking = function () {
+    console.log('Make the dough');
+    console.log('Put topping on the pizza');
+    console.log('Bake the pizza');
+};
+
+// Calling the function
+pizzaMaking();
+pizzaMaking();
+
+const makeSushi = function () {
+    console.log('Spread the seaweed');
+    console.log('Put the rice, raw fish and other ingredients in the seaweed');
+    console.log('Roll up the seaweed and cut the sushi roll into pieces');
+
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
