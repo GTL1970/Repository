348a700ef98b0260ac9a913e3ce1dd07acfb8e88
GTL1970 <92163683+GTL1970@users.noreply.github.com>
2021-12-03 21:36:53 +0100

    Add files via upload

--- grade-assigner.js
+++ grade-assigner.js
@@ -21,19 +21,20 @@ console.log("You scored a ", assignGrade(57));
 
 
 //for loop grade assigner
+function assignGrade(score) {
+    if (score > 90) {
+        return "A";
+    } else if (score > 80) {
+        return "B";
+    } else if (score > 70) {
+        return "C";
+    } else if (score > 65) {
+        return "D";
+    } else {
+        return "E";
+    }
+}
+
 for (var n = 60; n <= 100; n++) {
     console.log('For scoring ' + n + ', you get an ' + assignGrade(n));
-    function assignGrade(score) {
-        if (score > 90) {
-            return "A";
-        } else if (score > 80) {
-            return "B";
-        } else if (score > 70) {
-            return "C";
-        } else if (score > 65) {
-            return "D";
-        } else {
-            return "E";
-        }
-    }
-};
+}
