ac57d5db71e846a7c196eeba5e5929c985d20eb7
GTL1970 <92163683+GTL1970@users.noreply.github.com>
2021-12-15 09:48:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,91 @@
 // Guess the number
 
-// Input name and return message
-function nameEnter() {
-    var doc = prompt("Please enter your name",
-        "Name");
-    if (doc != null) {
-        document.getElementById("n").innerHTML =
-            "Hey, " + doc + " . Let's play a game!";
+// variables
+let numberToBeGuessed = null;
+let userName;
+let currentGuess;
+let numberOfGuesses = 5;
+let minNum = 0;
+let maxNum = 25;
+
+// Input name
+while (userName === undefined || userName === null || userName.length === 0) {
+    userName = prompt("Welcome! What is your name?");
+}
+// return answer
+alert("Hey " + userName + "! Let's play a game!");
+
+
+let selectedMinNum = parseInt(
+    prompt("Choose a whole number to set your minimum parameter (standard: 0)")
+);
+if (!isNaN(selectedMinNum) && selectedMinNum >= 0) {
+    minNum = selectedMinNum;
+}
+
+let selectedMaxNum = parseInt(
+    prompt("Choose a whole number to set your maximum parameter. (standard: 25, higher than " + minNum + " )")
+);
+if (!isNaN(selectedMaxNum)) {
+    maxNum = selectedMaxNum;
+}
+
+while (maxNum <= minNum) {
+    let selectedMaxNum = parseInt(
+        prompt("Choose a whole number to set your maximum parameter. (higher than " + minNum + " )")
+    );
+    if (!isNaN(selectedMaxNum)) {
+        maxNum = selectedMaxNum;
     }
 }
 
-// Random number picked by computer
-const min = 0;
-const max = 25;
-function randomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
+alert("You have 5 trials to guess the number. The number is between " + minNum + " and " + maxNum + ". Good luck!");
 
-// Enter a number between 0 and 25 to start guessing the number I have in mind
-function numberEnter() {
-    var doc = prompt("Enter a number between 0 and 25 to start guessing the number I have in mind",
-        "Number");
-    if (doc != null) {
-        document.getElementById("x").innerHTML =
-            "You typed number " + doc;
+numberToBeGuessed = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
+console.log("Pssst the number is: " + numberToBeGuessed);
+
+while (currentGuess !== numberToBeGuessed) {
+    if (numberOfGuesses < 1) {
+        alert(
+            "Sorry, You have exhausted your trials. You have lost. We will start again with a new number! The number is still between " + minNum + " and " + maxNum + "."
+        );
+        numberOfGuesses = 5;
+        numberToBeGuessed = Math.floor(Math.random() * (maxNum - minNum)) + minNum;
     }
-}
+    currentGuess = parseInt(prompt("Enter a number.."));
 
-// Compare numbers and if-else message loop: Enter number in loop
-if (numberEnter != randomNumber) {
-    document.write("Unfortunately, that is not correct");
-} else {
-    document.write("Congratulations, you won the game!");
-    document.write("Bye " + doc + ", see you!");
+    while (isNaN(currentGuess)) {
+        currentGuess = parseInt(
+            prompt(
+                "Your entry was not a number. Try again. Enter a number..."
+            )
+        );
+    }
+
+    alert("Your guess is: " + currentGuess);
+
+    if (currentGuess > numberToBeGuessed) {
+        numberOfGuesses--;
+        if (numberOfGuesses < 1) {
+            alert("This was your last trial.");
+        } else {
+            alert(
+                "Unfortunately, that is not correct! Your guess was too high. You have " + numberOfGuesses + " left. Try again. Enter a number..."
+            );
+        }
+    } else if (currentGuess < numberToBeGuessed) {
+        numberOfGuesses--;
+        if (numberOfGuesses < 1) {
+            alert("This was your last trial.");
+        } else {
+            alert(
+                "Unfortunately, that is not correct! Your guess was too low. You have " + numberOfGuesses + " left. Try again. Enter a number..."
+            );
+        }
+    } else {
+        alert("Congratulations, you have won the game! The number was: " + currentGuess);
+        alert(
+            "The game is over!. Thanks for playing " + userName + ". Bye, see you next time!"
+        );
+    }
 }
